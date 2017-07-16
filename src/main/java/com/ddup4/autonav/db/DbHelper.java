b1f3f760b42611f77dd5729d81e3ae6b0c3ce274
shelgeem <xiaoju1987yond@163.com>
2017-07-16 18:20:15 +0800

    add app config perperites

--- src/main/java/com/ddup4/autonav/db/DbHelper.java
+++ src/main/java/com/ddup4/autonav/db/DbHelper.java
@@ -1,37 +1,57 @@
 package com.ddup4.autonav.db;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.Properties;
+
+import org.springframework.core.io.ClassPathResource;
 
 public class DbHelper {
-	private static final String url = "jdbc:mysql://localhost:3306/db_autonav";
-	private static final String name = "com.mysql.jdbc.Driver";
-	private  static final String user = "root";
-	private static final String password = "root";
-	
-	
+
+	private static Properties appConfig = new Properties();
+	static {
+		InputStream is = null;
+		try {
+			is = new ClassPathResource("app_config.properties").getInputStream();
+			appConfig.load(is);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	private static final String url = appConfig.getProperty("mysql_url");
+	private static final String name = appConfig.getProperty("mysql_driver");
+	private static final String user = appConfig.getProperty("mysql_user");
+	private static final String password = appConfig.getProperty("mysql_password");
+
 	public Connection conn = null;
 	public PreparedStatement prepare = null;
-	
-	public DbHelper (String sql) {
+
+	public DbHelper(String sql) {
 		try {
 			Class.forName(name);
 			conn = DriverManager.getConnection(url, user, password);
 			prepare = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 		}
 	}
-	
+
 	public void close() {
 		try {
 			if (conn != null) {
 				conn.close();
 			}
-			if(prepare != null) {
+			if (prepare != null) {
 				prepare.close();
 			}
 		} catch (Exception e) {
